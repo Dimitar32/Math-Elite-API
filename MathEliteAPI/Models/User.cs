720c388abe78a07935181c47a7cf861c014cf0eb
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2024-12-14 11:08:13 +0200

    add model for user

---
+++ MathEliteAPI/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace MathEliteAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Provider { get; set; } // e.g., Google, Facebook
+        public string ProviderId { get; set; } // Unique ID from the provider
+    }
+}
